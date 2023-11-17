9d3fd8e6ac09799f31afa074cbe07391730e3313
shin <advensploit@gmail.com>
2023-11-17 11:01:59 +0800

    updated chartbar
    added api

--- src/Components/Chartbar/Chartbar.jsx
+++ src/Components/Chartbar/Chartbar.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import axios from 'axios';
 
 const fixedColor = '#00CED1'; // Warna yang tetap atau sesuaikan dengan kebutuhan Anda
 
 const Chartbar = () => {
-  const data = [
-    { name: 'Laptop', value: 30 },
-    { name: 'Smartphone', value: 80 },
-    { name: 'Tablet', value: 30 },
-    { name: 'Laptop', value: 30 },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Get the Authorization bearer token from localStorage
+    const token = localStorage.getItem('accessToken'); // replace 'your_token_key' with the actual key you used to store the token
+
+    // Fetch data from the API with Authorization header
+    axios.get('http://127.0.0.1:8000/api/count-barangkategori', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        const apiData = response.data;
+
+        // Transform the API data into the format expected by your chart
+        const chartData = apiData.map(item => ({
+          name: item.kategori,
+          value: item.total,
+        }));
+
+        // Set the chart data in the state
+        setData(chartData);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+  // Your chart rendering code here, using the 'data' state
 
   return (
     <div className='shadow-md pt-4 pr-4 border rounded-md bg-white'>
