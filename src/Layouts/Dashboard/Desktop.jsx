d7c83d0043517081954745e8c8a216839185c6d6
shin <advensploit@gmail.com>
2023-11-17 11:15:16 +0800

    reupdated

--- src/Layouts/Dashboard/Desktop.jsx
+++ src/Layouts/Dashboard/Desktop.jsx
@@ -1,182 +1,272 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import SidebarDesktop from "../../Components/Navigation/SidebarDesktop";
 import AdminName from "../../Components/AdminName/AdminName";
 import DashboardCard from "../../Components/DashboardCard/DashboardCard";
 import Chartbar from "../../Components/Chartbar/Chartbar";
-import {
-  FaChartBar,
-  FaUser,
-  FaBox,
-  FaClipboardList,
-  FaTools,
-} from "react-icons/fa";
-
-import { MdAssignment } from "react-icons/md";
+import {FaChartBar, FaUser, FaBox, FaClipboardList, FaTools} from "react-icons/fa";
+import axios from "axios";
+import {MdAssignment} from 'react-icons/md';
 
 const DashboardDesktop = () => {
-  const sidebarItems = [
-    {
-      text: "Dashboard",
-      icon: <FaChartBar />,
-      subtext: "Panel Informasi",
-      path: "/dashboard",
-    },
-    {
-      text: "Pengguna",
-      icon: <FaUser size={14} />,
-      subtext: "Menajemen Data",
-      path: "/account"
-    },
-    {
-      text: "Barang",
-      icon: <FaBox size={14} />,
-      path : "item"
-    },
-    {
-      text: "Peminjaman",
-      icon: <FaClipboardList />,
-      children: [
+    const sidebarItems = [
         {
-          sub: "Pengajuan",
-          path : "submission"
-        },
-        {
-          sub: "Peminjaman",
-          path : "borrow"
-        },
-        {
-          sub: "Pengembalian",
-          path : "return"
-        },
-      ],
-    },
-    {
-      text: "Utilitas",
-      icon: <FaTools />,
-      subtext: "Konfigurasi Web",
-      children: [
-        {
-          sub: "Jabatan",
-          path : "jobset"
-        },
-        {
-          sub: "Jurusan",
-          path : "majorset"
-        },
-        {
-          sub: "Kategori Barang",
-          path: "cataset"
-        },
-      ],
-    },
-  ];
+            text: "Dashboard",
+            icon: <FaChartBar/>,
+            subtext: "Panel Informasi",
+            path: "/dashd"
+        }, {
+            text: "Pengguna",
+            icon: <FaUser size={14}/>,
+            subtext: "Menajemen Data"
+        }, {
+            text: "Barang",
+            icon: <FaBox size={14}/>
+        }, {
+            text: "Peminjaman",
+            icon: <FaClipboardList/>,
+            children: [
+                {
+                    sub: "Pengajuan"
+                }, {
+                    sub: "Peminjaman"
+                }, {
+                    sub: "Pengembalian"
+                }
+            ]
+        }, {
+            text: "Utilitas",
+            icon: <FaTools/>,
+            subtext: "Konfigurasi Web",
+            children: [
+                {
+                    sub: "Jabatan"
+                }, {
+                    sub: "Jurusan"
+                }, {
+                    sub: "Kategori Barang"
+                }
+            ]
+        }
+    ];
 
-  const contentData = {
-    title: "Pengguna",
-    content: "Dashboard Content",
-    icon: FaUser,
-    pages: [
-      {
-        title: "Siswa",
-        content: "6/10",
-        link: "pengguna",
-      },
-      {
-        title: "Guru",
-        content: "4/10",
-        link: "pengguna",
-      },
-    ],
-  };
+    const [contentData, setContentData] = useState({
+        title: 'Pengguna',
+        content: 'Dashboard Content',
+        icon: FaUser,
+        pages: [
+          {
+            title: 'Siswa',
+            content: '', // No initial loading placeholder
+            link: 'pengguna',
+          },
+          {
+            title: 'Guru',
+            content: '',
+            link: 'pengguna',
+          },
+        ],
+      });
+      const [contentData2, setContentData2] = useState({
+        title: 'Barang',
+        content: 'Dashboard Content',
+        icon: FaBox,
+        pages: [
+          {
+            title: 'Tersedia',
+            content: '',
+          },
+          {
+            title: 'Dipinjam',
+            content: '',
+          },
+          {
+            title: 'Pemeliharaan',
+            content: '',
+          },
+          {
+            title: 'Dihapuskan',
+            content: '',
+          },
+          // Add other pages as needed
+        ],
+      });
+      const [contentData3, setContentData3] = useState({
+        title: 'Peminjaman',
+        content: 'Dashboard Content',
+        icon: MdAssignment,
+        pages: [
+          {
+            title: 'Permohonan',
+            content: '',
+          },
+          {
+            title: 'Peminjaman',
+            content: '',
+          },
+          {
+            title: 'Pengembalian',
+            content: '',
+          },
+          // Add other pages as needed
+        ],
+      });
+    
+    
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const token = localStorage.getItem('accessToken');
+    
+            // Fetch total counts for each type of user
+            const countPenggunaResponse = await axios.get('http://127.0.0.1:8000/api/count-pengguna', {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+    
+            const countPenggunaData = countPenggunaResponse.data;
+    
+            // Fetch data for tipepengguna
+            const countTipePenggunaResponse = await axios.get('http://127.0.0.1:8000/api/count-tipepengguna', {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+    
+            const countTipePenggunaData = countTipePenggunaResponse.data;
+    
+            // Update contentData with the fetched values
+            setContentData((prevContentData) => ({
+              ...prevContentData,
+              pages: [
+                {
+                  ...prevContentData.pages[0],
+                  content: `${countTipePenggunaData.total_siswa}/${countPenggunaData.total}`,
+                },
+                {
+                  ...prevContentData.pages[1],
+                  content: `${countTipePenggunaData.total_guru}/${countPenggunaData.total}`,
+                },
+              ],
+            }));
+    
+            // Fetch data for tipebarang
+            const countTipeBarangResponse = await axios.get('http://127.0.0.1:8000/api/count-tipebarang', {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+            const countBarangResponse = await axios.get('http://127.0.0.1:8000/api/count-barang', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+      
+    
+            const countTipeBarangData = countTipeBarangResponse.data;
+            const countBarangData = countBarangResponse.data;
+            // Update contentData2 with the fetched values
+            setContentData2((prevContentData2) => ({
+              ...prevContentData2,
+              pages: [
+                {
+                  ...prevContentData2.pages[0],
+                  content: `${countTipeBarangData.total_baik}/${countBarangData.total}`,
+                },
+                {
+                  ...prevContentData2.pages[1],
+                  content: `${countTipeBarangData.total_rusak}/${countBarangData.total}`,
+                },
+                {
+                  ...prevContentData2.pages[2],
+                  content: `${countTipeBarangData.total_pemeliharaan}/${countBarangData.total}`,
+                },
+                {
+                  ...prevContentData2.pages[3],
+                  content: `${countTipeBarangData.total_dihapuskan}/${countBarangData.total}`,
+                },
+                // Add other pages as needed
+              ],
+            }));
+            const countPermohonanResponse = await axios.get('http://127.0.0.1:8000/api/count-permohonan', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+      
+              const countPermohonanData = countPermohonanResponse.data;
+      
+              // Fetch data for peminjaman
+              const countPeminjamanResponse = await axios.get('http://127.0.0.1:8000/api/count-peminjaman', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+      
+              const countPeminjamanData = countPeminjamanResponse.data;
+      
+              // Fetch data for pengembalian
+              const countPengembalianResponse = await axios.get('http://127.0.0.1:8000/api/count-pengembalian', {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+      
+              const countPengembalianData = countPengembalianResponse.data;
+      
+              // Update contentData3 with the fetched values
+              setContentData3((prevContentData3) => ({
+                ...prevContentData3,
+                pages: [
+                  {
+                    ...prevContentData3.pages[0],
+                    content: `${countPermohonanData.total}`,
+                  },
+                  {
+                    ...prevContentData3.pages[1],
+                    content: `${countPeminjamanData.total}`,
+                  },
+                  {
+                    ...prevContentData3.pages[2],
+                    content: `${countPengembalianData.total}`,
+                  },
+                  // Add other pages as needed
+                ],
+              }));
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+          };
+      
+          fetchData(); // Call the async function
+        }, []); // Empty dep
+   
 
-  const contentData2 = {
-    title: "Barang",
-    content: "Dashboard Content",
-    icon: FaBox,
-    pages: [
-      {
-        title: "Tersedia",
-        content: "12",
-      },
-      {
-        title: "Dipinjam",
-        content: "12",
-      },
-      {
-        title: "Pemeliharaan",
-        content: "12",
-      },
-      {
-        title: "Dihapuskan",
-        content: "12",
-      },
-      // Tambahkan halaman lain sesuai kebutuhan
-    ],
-  };
 
-  const contentData3 = {
-    title: "Peminjaman",
-    content: "Dashboard Content",
-    icon: MdAssignment, // Sesuaikan dengan ikon yang ingin ditampilkan
-    pages: [
-      {
-        title: "Pengajuan",
-        content: "12",
-      },
-      {
-        title: "Peminjaman",
-        content: "12",
-      },
-      {
-        title: "Pengembalian",
-        content: "12",
-      },
-      // Tambahkan halaman lain sesuai kebutuhan
-    ],
-  };
-
-  const data1 = [
-    {
-      name: "Guru",
-      value: 30,
-    },
-    {
-      name: "Siswa",
-      value: 80,
-    },
-  ];
-
-  return (
-    <div className=" flex">
-      <SidebarDesktop items={sidebarItems} />
-      <div className=" px-8 py-4 min-h-screen w-screen">
-        <AdminName storageKey="user_name" />
-        <div className="flex flex-wrap -mx-4">
-          <DashboardCard
-            contentData={contentData}
-            iconColor="red"
-            titleTextColor="red"
-          />{" "}
-          {/* Gunakan komponen DashboardCard dengan warna ikon biru dan teks judul pink */}
-          <DashboardCard
-            contentData={contentData3}
-            iconColor="pink"
-            titleTextColor="pink"
-          />
-          <DashboardCard
-            contentData={contentData2}
-            iconColor="blue"
-            titleTextColor="blue"
-          />
-          <div className=" px-6">
-            <center>
-              <Chartbar />
-            </center>
-          </div>
+    return (
+        <div className=" flex">
+            <SidebarDesktop items={sidebarItems}/>
+            <div className=" px-8 py-4 min-h-screen w-screen">
+                <AdminName storageKey="user_name"/>
+                <div className="flex flex-wrap -mx-4">
+                    <DashboardCard contentData={contentData} iconColor="red" titleTextColor="red"/> {/* Gunakan komponen DashboardCard dengan warna ikon biru dan teks judul pink */}
+                    <DashboardCard
+                        contentData={contentData3}
+                        iconColor="pink"
+                        titleTextColor="pink"/>
+                    <DashboardCard
+                        contentData={contentData2}
+                        iconColor="blue"
+                        titleTextColor="blue"/>
+                        <div className=" px-6">
+                        <center>
+                        <Chartbar/>
+                        </center>
+                        </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DashboardDesktop;
--- src/Layouts/Dashboard/Mobile.jsx
+++ src/Layouts/Dashboard/Mobile.jsx
@@ -113,65 +113,197 @@ const DashboardMobile = () => {
     },
   };
 
-  const contentData = {
-    title: "Pengguna",
-    content: "Dashboard Content",
+  const [contentData, setContentData] = useState({
+    title: 'Pengguna',
+    content: 'Dashboard Content',
     icon: FaUser,
     pages: [
       {
-        title: "Siswa",
-        content: "6/10",
+        title: 'Siswa',
+        content: '', // No initial loading placeholder
+        link: 'pengguna',
       },
       {
-        title: "Guru",
-        content: "4/10",
+        title: 'Guru',
+        content: '',
+        link: 'pengguna',
       },
     ],
-  };
-
-  const contentData2 = {
-    title: "Barang",
-    content: "Dashboard Content",
+  });
+  const [contentData2, setContentData2] = useState({
+    title: 'Barang',
+    content: 'Dashboard Content',
     icon: FaBox,
     pages: [
       {
-        title: "Tersedia",
-        content: "12",
+        title: 'Tersedia',
+        content: '',
       },
       {
-        title: "Dipinjam",
-        content: "12",
+        title: 'Dipinjam',
+        content: '',
       },
       {
-        title: "Pemeliharaan",
-        content: "12",
+        title: 'Pemeliharaan',
+        content: '',
       },
       {
-        title: "Dihapuskan",
-        content: "12",
+        title: 'Dihapuskan',
+        content: '',
       },
+      // Add other pages as needed
     ],
-  };
-
-  const contentData3 = {
-    title: "Peminjaman",
-    content: "Dashboard Content",
+  });
+  const [contentData3, setContentData3] = useState({
+    title: 'Peminjaman',
+    content: 'Dashboard Content',
     icon: MdAssignment,
     pages: [
       {
-        title: "Pengajuan",
-        content: "12",
+        title: 'Permohonan',
+        content: '',
       },
       {
-        title: "Peminjaman",
-        content: "12",
+        title: 'Peminjaman',
+        content: '',
       },
       {
-        title: "Pengembalian",
-        content: "12",
+        title: 'Pengembalian',
+        content: '',
       },
+      // Add other pages as needed
     ],
-  };
+  });
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem('accessToken');
+
+        // Fetch total counts for each type of user
+        const countPenggunaResponse = await axios.get('http://127.0.0.1:8000/api/count-pengguna', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const countPenggunaData = countPenggunaResponse.data;
+
+        // Fetch data for tipepengguna
+        const countTipePenggunaResponse = await axios.get('http://127.0.0.1:8000/api/count-tipepengguna', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const countTipePenggunaData = countTipePenggunaResponse.data;
+
+        // Update contentData with the fetched values
+        setContentData((prevContentData) => ({
+          ...prevContentData,
+          pages: [
+            {
+              ...prevContentData.pages[0],
+              content: `${countTipePenggunaData.total_siswa}/${countPenggunaData.total}`,
+            },
+            {
+              ...prevContentData.pages[1],
+              content: `${countTipePenggunaData.total_guru}/${countPenggunaData.total}`,
+            },
+          ],
+        }));
+
+        // Fetch data for tipebarang
+        const countTipeBarangResponse = await axios.get('http://127.0.0.1:8000/api/count-tipebarang', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const countBarangResponse = await axios.get('http://127.0.0.1:8000/api/count-barang', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+
+        const countTipeBarangData = countTipeBarangResponse.data;
+        const countBarangData = countBarangResponse.data;
+        // Update contentData2 with the fetched values
+        setContentData2((prevContentData2) => ({
+          ...prevContentData2,
+          pages: [
+            {
+              ...prevContentData2.pages[0],
+              content: `${countTipeBarangData.total_baik}/${countBarangData.total}`,
+            },
+            {
+              ...prevContentData2.pages[1],
+              content: `${countTipeBarangData.total_rusak}/${countBarangData.total}`,
+            },
+            {
+              ...prevContentData2.pages[2],
+              content: `${countTipeBarangData.total_pemeliharaan}/${countBarangData.total}`,
+            },
+            {
+              ...prevContentData2.pages[3],
+              content: `${countTipeBarangData.total_dihapuskan}/${countBarangData.total}`,
+            },
+            // Add other pages as needed
+          ],
+        }));
+        const countPermohonanResponse = await axios.get('http://127.0.0.1:8000/api/count-permohonan', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          const countPermohonanData = countPermohonanResponse.data;
+  
+          // Fetch data for peminjaman
+          const countPeminjamanResponse = await axios.get('http://127.0.0.1:8000/api/count-peminjaman', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          const countPeminjamanData = countPeminjamanResponse.data;
+  
+          // Fetch data for pengembalian
+          const countPengembalianResponse = await axios.get('http://127.0.0.1:8000/api/count-pengembalian', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          const countPengembalianData = countPengembalianResponse.data;
+  
+          // Update contentData3 with the fetched values
+          setContentData3((prevContentData3) => ({
+            ...prevContentData3,
+            pages: [
+              {
+                ...prevContentData3.pages[0],
+                content: `${countPermohonanData.total}`,
+              },
+              {
+                ...prevContentData3.pages[1],
+                content: `${countPeminjamanData.total}`,
+              },
+              {
+                ...prevContentData3.pages[2],
+                content: `${countPengembalianData.total}`,
+              },
+              // Add other pages as needed
+            ],
+          }));
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData(); // Call the async function
+    }, []); // Empty dep
 
   return (
     <div ref={sidebarRef}>
