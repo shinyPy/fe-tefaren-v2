98f4bcce21143b820a35b39c2d5f1fef978a3740
shin <71328419+shinyPy@users.noreply.github.com>
2024-03-04 23:44:17 +0800

    analytics v2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from '@vercel/analytics/react';
 
 import './Assets/Style/App.css';
 
@@ -9,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
 
